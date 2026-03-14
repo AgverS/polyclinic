3af3a3383e6baeab34b353c93615a430eea5c060
AgverS <annasychova23@yandex.by>
2026-03-14 20:10:23 +0300

    fix: changed backet, pharmacy

--- lib/context.tsx
+++ lib/context.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { User } from "@/lib/generated/prisma";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useEffect, useContext, useState } from "react";
 import { decodeUserFromToken } from "./jwt";
 
 type AuthContextType = {
